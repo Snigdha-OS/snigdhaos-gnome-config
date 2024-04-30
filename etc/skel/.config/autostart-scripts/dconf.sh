231d2c279f2c244dd6a465713f4c3751ed41336d
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-30 13:28:58 +0530

    feat: add  dconf loader script

---
+++ etc/skel/.config/autostart-scripts/dconf.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+dconf load / < ~/.config/snigdhaos-dconf.ini
+rm -f ~/.config/snigdhaos-dconf.ini ~/.config/autostart-scripts/dconf.sh &
+ 
+notify-send "Snigdha OS Arctic V settings applied! 🔥"
