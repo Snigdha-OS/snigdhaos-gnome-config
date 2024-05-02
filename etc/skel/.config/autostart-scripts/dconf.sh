e4117c1b73c264544204ffbc70bb5a7e6cb5f7d1
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-05-02 13:51:31 +0530

    feat(users): add our own config

--- etc/skel/.config/autostart-scripts/dconf.sh
+++ etc/skel/.config/autostart-scripts/dconf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dconf load / < ~/.config/snigdhaos-dconf.ini
-rm -f ~/.config/snigdhaos-dconf.ini ~/.config/autostart-scripts/dconf.sh &
+dconf load / < ~/.config/snigdhaos-gnome-config.ini
+rm -f ~/.config/snigdhaos-gnome-config.ini ~/.config/autostart-scripts/dconf.sh &
  
 notify-send "Snigdha OS Arctic V settings applied! 🔥"
